2be7e0f81f0f969555f6a198abd40ddaefc9f71f
htfab <mpw@htamas.net>
2025-09-09 07:02:48 +0200

    add cells/build_all.sh

---
+++ cells/build_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+pushd cell0
+./build.py
+popd
+pushd cell1
+./build.py
+popd
+pushd cell2
+./update.sh
+popd
+pushd cell3
+./update.sh
+popd
+pushd cell4
+./build.py
+popd
+pushd cell5
+./build.py
+popd
+pushd cell6
+./build.py
+popd
+pushd cell7
+./build.py
+popd
